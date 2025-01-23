fc54fa375af6645edd767a974cd3bb3a192d2f18
abhishekn211 <150190422+abhishekn211@users.noreply.github.com>
2025-01-23 13:40:11 +0530

    Update BookCard.jsx

--- frontend/src/pages/books/BookCard.jsx
+++ frontend/src/pages/books/BookCard.jsx
@@ -36,7 +36,7 @@ const BookCard = ({book}) => {
                     </Link>
                     <p className="text-gray-600 mb-5">{book?.description.length > 80 ? `${book.description.slice(0, 80)}...` : book?.description}</p>
                     <p className="font-medium mb-5">
-                        ${book?.newPrice} <span className="line-through font-normal ml-2">$ {book?.oldPrice}</span>
+                        Rs{book?.newPrice} <span className="line-through font-normal ml-2">Rs {book?.oldPrice}</span>
                     </p>
                     <button 
                     onClick={() => handleAddToCart(book)}
