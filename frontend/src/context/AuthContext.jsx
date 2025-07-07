81c7d368c0d001a02840506ce2bfb3e38bb61f27
abhishekn211 <150190422+abhishekn211@users.noreply.github.com>
2025-07-07 22:51:50 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -50,7 +50,7 @@ export const AuthProvide = ({children}) => {
                 const userData = {
                     email, username: displayName, photo: photoURL
                 } 
-                localStorage.setitem('userData',userData);
+                localStorage.setItem('userData',userData);
             }
         })
 
