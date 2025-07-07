dd47d6dcb4e615690a813517a6762baa95598813
abhishekn211 <150190422+abhishekn211@users.noreply.github.com>
2025-07-07 16:49:33 +0530

    Update DashboardLayout.jsx

--- frontend/src/pages/dashboard/DashboardLayout.jsx
+++ frontend/src/pages/dashboard/DashboardLayout.jsx
@@ -72,7 +72,7 @@ const DashboardLayout = () => {
           <button className="inline-flex items-center p-2 hover:bg-gray-100 focus:bg-gray-100 rounded-lg">
             <span className="sr-only">User Menu</span>
             <div className="hidden md:flex md:flex-col md:items-end md:leading-tight">
-              <span className="font-semibold">Abhishek Nigam</span>
+              <span className="font-semibold">Kashish Bhatia</span>
               <span className="text-sm text-gray-600">SDE-II</span>
             </div>
             <span className="h-12 w-12 ml-2 sm:ml-3 mr-2 bg-gray-100 rounded-full overflow-hidden">
